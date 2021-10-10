d57ef86ae318d9e9a4b77fd35874a10b1dd36253
ilfat Aliev <ialiev685@gmail.com>
2021-10-10 21:14:27 +0300

    hw done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -57,38 +57,37 @@ router.delete('/:contactId', async (req, res, next) => {
 router.patch('/:contactId', async (req, res, next) => {
   const { contactId } = req.params
 
-  const obj = Object.keys(req.body)
-  if (obj.length === 0) {
-    res.status(400).json({ message: 'missing fields' })
-    return
-  }
-
   const shema = Joi.object({
     name: Joi.string()
       .alphanum()
       .min(2)
       .max(10)
-      .allow(''),
+      .allow('')
+      .required(),
     email: Joi.string()
       .email()
-      .allow(''),
+      .allow('')
+      .required(),
     phone: Joi.number()
       .integer()
       .min(89000000000)
       .max(89999999999)
-      .allow(''),
+      .allow('')
+      .required(),
 
   })
 
-  const { error } = shema.validate(req.body)
+  const { error, value } = shema.validate(req.body)
+  console.log(value)
 
   if (error) {
     const text = error?.details[0].message.replace(/["]/g, '')
     res.status(400).json({ message: text })
     return
   }
-
+  const obj = Object.keys(value)
   const updateItems = {}
+
   for (const key of obj) {
     if (req.body[key].trim().length !== 0) updateItems[key] = req.body[key]
   }
