4f1842a9cd1f9097abfca43c33c39ace0b956750
ilfat Aliev <ialiev685@gmail.com>
2021-10-19 21:48:03 +0300

    получение, обновление контактов выполнено

--- model/contacts/getContactById.js
+++ model/contacts/getContactById.js
@@ -1,13 +1,19 @@
-const fs = require('fs/promises')
-const path = require('path')
-const contacts = path.resolve('db', 'contacts.json')
-const normalizationData = require('./normalizationData')
+// const fs = require('fs/promises')
+// const path = require('path')
+// const contacts = path.resolve('db', 'contacts.json')
+// const normalizationData = require('./normalizationData')
+
+const Contact = require('../schemas/contact')
 
 const getContactById = async (contactId) => {
   try {
-    const data = await fs.readFile(contacts)
+    // const data = await fs.readFile(contacts)
 
-    return normalizationData(data).find(({ id }) => id === Number(contactId))
+    const data = await Contact.findOne({ _id: contactId })
+
+    // return normalizationData(data).find(({ id }) => id === Number(contactId))
+
+    return data
   } catch (error) {
     console.log(error)
   }
--- model/contacts/updateContact.js
+++ model/contacts/updateContact.js
@@ -1,22 +1,14 @@
-const fs = require('fs/promises')
-const path = require('path')
-const contacts = path.resolve('db', 'contacts.json')
-const normalizationData = require('./normalizationData')
+// const fs = require('fs/promises')
+// const path = require('path')
+// const contacts = path.resolve('db', 'contacts.json')
+// const normalizationData = require('./normalizationData')
+const Contact = require('../schemas/contact')
 
 const updateContact = async (contactId, body) => {
   try {
-    const data = await fs.readFile(contacts)
-    let newContact = null
-    const updateContacts = normalizationData(data).map((el) => {
-      if (el.id === Number(contactId)) {
-        newContact = { ...el, ...body }
-        return { ...el, ...body }
-      }
-      return el
-    })
-    await fs.writeFile(contacts, JSON.stringify([...updateContacts]))
-
-    return newContact
+    const data = await Contact.findOneAndUpdate({ _id: contactId }, { ...body }, { new: true })
+    console.log(data)
+    return data
   } catch (error) {
     console.log(error)
   }
