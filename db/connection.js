333542062f8396e1a6db0bff879fbebebeedaf53
ilfat Aliev <ialiev685@gmail.com>
2021-10-22 23:23:13 +0300

    доработка конфендициальной информации

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const DB_HOST = process.env.DB_HOST
+const { DB_USER, DB_USER_PASS, DB_NAME } = process.env
+console.log(DB_USER, DB_USER_PASS, DB_NAME)
+// DB_USER=ialiev
+// DB_USER_PASS=7256
+// DB_NAME=dbContacts
+
+const DB_HOST = `mongodb+srv://${DB_USER}:${DB_USER_PASS}@cluster0.kvh72.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 
 const connectMongoDb = async () => {
   return mongoose.connect(DB_HOST, {
