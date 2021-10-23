538fe777da17d1f259e898575950da57b2792741
ilfat Aliev <ialiev685@gmail.com>
2021-10-24 00:08:44 +0300

    обработка запроса на логирование завершена

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -6,6 +6,11 @@ const loginController = async (req, res, next) => {
   const { status } = result
 
   if (status === 401) res.status(400).json({ message: 'Email or password is wrong' })
+  if (status === 200) {
+    const { token, email, subscription } = result.data
+
+    res.status(200).json({ token, user: { email, subscription } })
+  }
 }
 
 module.exports = loginController
--- model/users/loginUser.js
+++ model/users/loginUser.js
@@ -1,20 +1,32 @@
 const User = require('../schemas/users.js')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const loginUser = async(body) => {
   const { email, password } = body
 
   const result = await User.findOne({ email }, '_id, password')
 
-  if (result) {
-    const hashPassword = result.password
-    const checkPassword = bcrypt.compareSync(password, hashPassword)
-
-    console.log(checkPassword)
-    
-    return { status: 200 }
+  if (!result) {
+    return { status: 401 }
   }
-  return { status: 401 }
+
+  const hashPassword = result.password
+  const checkPassword = bcrypt.compareSync(password, hashPassword)
+
+  if (!checkPassword) {
+    return { status: 401 }
+  }
+
+  const { SECRET_KEY } = process.env
+  const payload = {
+    id: result._id
+  }
+  const token = jwt.sign(payload, SECRET_KEY)
+
+  const data = await User.findByIdAndUpdate(payload.id, { token })
+
+  return { status: 200, data }
 }
 
 module.exports = loginUser
